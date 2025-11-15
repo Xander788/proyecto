fb2c8e19d8f679d8b64298cfb011f529633d3a89
Xander <alramirezba@est.utn.ac.cr>
2025-11-15 00:05:29 -0600

    Creacion de GestorBingo y el Interface de este
    Se crea el interface IGestorBingo para crear las funciones que se utilizaran en GestorBingo.
    
    Se crea la clase GestorBingo que implementa los metodos del interface y sus metodos son:
    
    anadirCarton(CartonBingo carton): anade un carton al list cartones
    void eliminarCarton(String id): elimina el carton
    List<CartonBingo> getCartones(): retorna el list cartones
    void setModoJuego(String modo): cambia el modo de juego
    String getModoJuego(): retorna el modo de juego en string
    void llamarNumero(int num): si en el hash numeros no esta el numero se agrega al hash y se marca en el carton
    void desmarcarNumero(int num): se remueve el numero y se remueve de el carton
    Set<Integer> getNumerosSacados(): retorna los numeros sacados
    boolean revisarVictoria(CartonBingo carton): checkea con otras funciones las posiciones marcadas de el carton y si en caso de cumplir alguna funcion de checkear retorna un booleano en true
    void reiniciarJuego(): limpia el hash numerossacados, la tombola y el carton
    int sacarNumero(): saca un numero de la tombola y lo retorna
    
    Metodos creados en la clase:
    
    checkHorizontal: checkea las posiciones en horizontal y si se cumple el caso de que las 5 casillas sean esten sacadas se retorna un booleano true
    
    checkVertical: checkea las posiciones en Verticaly si se cumple el caso de que las 5 casillas sean esten sacadas se retorna un booleano true
    
    checkDiagonal: checkea las posiciones en Diagonaly si se cumple el caso de que las 5 casillas sean esten sacadas se retorna un booleano true
    
    checkCuatroEsquinas: checkea las posiciones en CuatroEsquinas y si se cumple el caso de que las 4 casillas sean esten sacadas se retorna un booleano true
    
    checkCartonLleno: checkea que todo el carton este lleno y si se cumple esto se retorna un booleano true

---
+++ src/Modelo/GestorBingoMem.java
@@ -0,0 +1,162 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ *
+ * @author pxand
+ */
+public class GestorBingoMem implements IGestorBingo{
+    private List<CartonBingo> cartones = new ArrayList<>();
+    private Set<Integer> numerosSacados = new HashSet<>();
+    private Tombola tombola = Tombola.getInstance();
+    private String modoJuego = "normal";
+    
+    @Override
+    public void anadirCarton(CartonBingo carton) {
+        cartones.add(carton);
+    }
+
+    @Override
+    public void eliminarCarton(String id) {
+        CartonBingo encontrado = null;
+
+        for (CartonBingo c : cartones) {
+            if (c.getId().equals(id)) {
+                encontrado = c;
+                break;
+            }
+        }
+
+        if (encontrado != null) {
+            cartones.remove(encontrado);
+        }
+    }
+
+    @Override
+    public List<CartonBingo> getCartones() {
+        return cartones;
+    }
+
+    @Override
+    public void setModoJuego(String modo) {
+        this.modoJuego = modo;
+    }
+
+    @Override
+    public String getModoJuego() {
+        return modoJuego;
+    }
+
+    @Override
+    public void llamarNumero(int num) {
+        if (!numerosSacados.contains(num)) {
+            numerosSacados.add(num);
+            for (CartonBingo carton : cartones) {
+                carton.marcarNumero(num);
+            }
+        }
+    }
+
+    @Override
+    public void desmarcarNumero(int num) {
+        numerosSacados.remove(num);
+        for (CartonBingo carton : cartones) {
+            carton.desmarcarNumero(num);
+        }
+    }
+
+    @Override
+    public Set<Integer> getNumerosSacados() {
+        return numerosSacados;
+    }
+       
+
+    @Override
+    public boolean revisarVictoria(CartonBingo carton) {
+        boolean[][] sacado = carton.getMarcados();
+        switch (modoJuego) {
+            case "Normal":
+                return checkHorizontal(sacado) || checkVertical(sacado) || checkDiagonal(sacado) || checkCuatroEsquinas(sacado);
+            case "Cuatro Esquinas":
+                return checkCuatroEsquinas(sacado);
+            case "Carton Lleno":
+                return checkCartonLleno(sacado);
+            default:
+                return false;
+        }
+    }
+
+    @Override
+    public void reiniciarJuego() {
+        numerosSacados.clear();
+        tombola.reiniciar();
+        for (CartonBingo carton : cartones) {
+            carton.reiniciarCarton();
+        }
+    }
+
+    @Override
+    public int sacarNumero() {
+        return tombola.sacarNumero();
+    }
+    
+    private boolean checkHorizontal(boolean[][] sacado) {
+        for (int i = 0; i < 5; i++) {
+            boolean win = true;
+            for (int j = 0; j < 5; j++) {
+                if (!sacado[i][j]) {
+                    win = false;
+                    break;
+                }
+            }
+            if (win) return true;
+        }
+        return false;
+    }
+
+    private boolean checkVertical(boolean[][] sacado) {
+        for (int j = 0; j < 5; j++) {
+            boolean win = true;
+            for (int i = 0; i < 5; i++) {
+                if (!sacado[i][j]) {
+                    win = false;
+                    break;
+                }
+            }
+            if (win) return true;
+        }
+        return false;
+    }
+
+    private boolean checkDiagonal(boolean[][] sacado) {
+        boolean win1 = true, win2 = true;
+        for (int i = 0; i < 5; i++) {
+            if (!sacado[i][i]) win1 = false;
+            if (!sacado[i][4 - i]) win2 = false;
+        }
+        return win1 || win2;
+    }
+
+    private boolean checkCuatroEsquinas(boolean[][] sacado) {
+        return sacado[0][0] && sacado[0][4] && sacado[4][0] && sacado[4][4];
+    }
+
+    private boolean checkCartonLleno(boolean[][] sacado) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (i == 2 && j == 2) continue;
+                if (!sacado[i][j]) return false;
+            }
+        }
+        return true;
+    }
+    
+}
