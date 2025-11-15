0c74efa5c62e092c9cc3e1e35348bed791dd141c
Danny Laptop <dachavesva@est.utn.ac.cr>
2025-11-15 10:06:38 -0600

    Clase TipoCartonFactory
    contiene  el factory method para
    crear instancias de fábricas de cartones según el modo de juego seleccionado. Recibe un parámetro modo y retorna
    
    si lo que esta retornando es automatico este genera los cartones autmaticos y sino genera los manuales

--- src/Modelo/TipoCartonFactory.java
+++ src/Modelo/TipoCartonFactory.java
@@ -9,5 +9,16 @@ package Modelo;
  * @author danny
  */
 public class TipoCartonFactory {
+    public static ICartonFactory getFactory(String modo){
+        if("automatico".equals(modo)){
+            return new CartonAutomaticoFactoryM();
+        }else{
+            return new CartonManualFactory();
+        }
+    }
     
 }
+// contiene  el factory method para
+//crear instancias de fábricas de cartones según el
+//modo de juego seleccionado. Recibe un parámetro
+//modo y retorna
